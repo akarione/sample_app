2eb3119b02663cfffa3ee73547c56f6fd15de39c
Mohd Ridhwan <mdrwan93@gmail.com>
2015-05-28 22:09:18 +0000

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 }
 
     before_save { email.downcase! }
-
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, 
@@ -12,5 +11,4 @@ class User < ActiveRecord::Base
                                 uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
-
 end
