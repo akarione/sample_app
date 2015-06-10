55b5e37db3dbbd02d137bc8c3ff58c0c48891e1b
Mohd Ridhwan <mdrwan93@gmail.com>
2015-06-10 17:46:15 +0000

    Set up login

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150525102047) do
-ActiveRecord::Schema.define(version: 20150525162148) do
+ActiveRecord::Schema.define(version: 20150608132011) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
