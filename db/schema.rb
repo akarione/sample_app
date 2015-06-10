3a1829b8f55dd850a59eb6f5d5f5ba221c927690
Mohd Ridhwan <mdrwan93@gmail.com>
2015-06-10 18:50:13 +0000

    Finish edit user signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150525102047) do
 ActiveRecord::Schema.define(version: 20150525162148) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
